762223e34efe79e1402f8ed0bfccc8c39f1fea7c
FORNALI Damien <itsdamoy@gmail.com>
2018-05-06 00:44:03 +0200

    Start level

--- js/world.js
+++ js/world.js
@@ -29,7 +29,7 @@ class World{
 		// leveling
 		this.levels = [];
 		this.currentLevel = null;
-		this.currentLevelId = 2;
+		this.currentLevelId = 0;
 		this.portals = [];
 
 		println("World: OK.");
